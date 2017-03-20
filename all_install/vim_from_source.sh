4535a9f7bac29f4238f5674ad917687b661476fc
Szymon Bugaj <s.bugaj@samsung.com>
2017-03-20 01:41:24 +0100

    *** automatic sync commit ***

--- all_install/vim_from_source.sh
+++ all_install/vim_from_source.sh
@@ -2,9 +2,11 @@ cd $_sources
 
 [[ ! -d vim ]] && git clone https://github.com/vim/vim.git 
 sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
-	libgtk3.0-dev libatk1.0-dev libbonoboui2-dev \
+	# libgtk3.0-dev \
+	libatk1.0-dev libbonoboui2-dev \
 	libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
 	python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git	
+exit
 sudo apt-get remove vim vim-runtime gvim
 cd vim
 ./configure --with-features=huge \
@@ -16,7 +18,8 @@ cd vim
 			# --with-python3-config-dir=/usr/lib/python3.5/config \
 			--enable-perlinterp=yes \
 			--enable-luainterp=yes \
-			--enable-gui=gtk3 --enable-cscope --prefix=/usr
+			# --enable-gui=gtk3 \
+			--enable-cscope --prefix=/usr
 cd src
 make
 sudo make install
