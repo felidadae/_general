4775c4e9f82a3d26e05a88cfb67f8342383de661
Szymon Bugaj <s.bugaj@samsung.com>
2017-08-09 16:01:54 +0200

    *** automatic sync commit ***

--- felidadaeTools/trip_weight_calculator/calculate.py
+++ felidadaeTools/trip_weight_calculator/calculate.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """
     simple tool to calculate weight of backback
+
+    @todo:
+    - fold by category, or parent_part
 """
 
 def apply_fun(fun, fun_state):
@@ -15,6 +18,10 @@ def apply_fun(fun, fun_state):
             fun(header, vals, fun_state)
 
     return fun_state
+
+
+def flat_print(list):
+    return ("\n".join(list))
 
 
 def get_options():
@@ -43,12 +50,8 @@ def calculate_taken():
     return state_
 
 
-def flat_print(list):
-    return ("\n".join(list))
-
-
 def calculate_taken_with_option(options, ignore_categories=set()):
-    state_ = { "sum": 0, "items": [] }
+    state_ = { "sum": 0, "items": [], "backpack_weight": 0 }
 
     def sum_options(header, vals, state):
         def get_col_val(col_name):
@@ -62,27 +65,48 @@ def calculate_taken_with_option(options, ignore_categories=set()):
         opt_v = get_col_val("option_value")
 
         if not cat in ignore_categories:
-            if ift:
+            if ift == "True":
                 if (opt_n not in options_k) or (opt_n in options_k and opt_v == options[opt_n]):
-                    state["items"].append("name: {}-{}-{}-{}".format(
-                        get_col_val("category"),
-                        get_col_val("parent_part"),
-                        get_col_val("part_name"),
-                        get_col_val("option_value")))
-                    state_["sum"] += int(get_col_val("weight")) * int(get_col_val("count"))
+                    if not get_col_val("weight") == "N/A":
+                        name = "{}-{}-{}-{}".format(
+                            get_col_val("category"),
+                            get_col_val("parent_part"),
+                            get_col_val("part_name"),
+                            get_col_val("option_value"))
+                        weight = int(get_col_val("weight"))
+                        state["items"].append((name, weight))
+                        state_["sum"] += int(get_col_val("weight")) * int(get_col_val("count"))
+                        if get_col_val("part_name") == "backpack": state_["backpack_weight"] = int(get_col_val("weight"))
 
     apply_fun(sum_options, state_) 
-    state_["to_print"] = "Options: {}\nIgnored categories: {}\nSum: {}kg\n{}".format(
-        options, ignore_categories, state_["sum"]/1000.0, flat_print(state_["items"]))
+
+    # sort items
+    state_["items"] = sorted(state_["items"], reverse=True, key=lambda item: item[1]) 
+
+    state_["to_print"] = "Options: {}\nIgnored categories: {}\nSum: {}kg ({}% of my body)\nSum without backpack: {}kg ({}% of my body)\n{}".format(
+        options, 
+        ignore_categories,
+		round(state_["sum"]/1000.0, 1),
+        round(state_["sum"]/1000.0/79*100, 1),
+		round((state_["sum"] - state_["backpack_weight"])/1000.0, 1),
+        round((state_["sum"] - state_["backpack_weight"])/1000.0/79*100, 1),
+        flat_print(
+            [
+                "name: {} weight: {} weight_all: {}%".format(
+                    name, weight, round(weight/(state_["sum"]-state_["backpack_weight"])*100,2)) for name, weight in state_["items"]
+            ]
+        )
+    )
     return state_
          
 
 if __name__ == "__main__":
     r = calculate_taken_with_option(
         options = {
-            "jacket-choice": "new", 
+            "jacket-choice": "german", 
             "laptop-choice": "surface", 
+            "tent-choice": "biwi-german",
             "burner-option": "solid-fuel"
         },
-        ignore_categories = set())
+        ignore_categories = set(["computer"]))
     print(r["to_print"])
