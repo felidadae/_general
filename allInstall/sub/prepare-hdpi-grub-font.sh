f5070e467caa2a83b1dc0e7b55d5379e3c3bc861
Szymon Bugaj <s.bugaj@samsung.com>
2017-08-29 10:18:27 +0200

    Move prepare-hdpi-grub-font to allInstall

--- allInstall/sub/prepare-hdpi-grub-font.sh
+++ allInstall/sub/prepare-hdpi-grub-font.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -e
 sudo grub-mkfont --output=/boot/grub/fonts/DejaVuSansMono24.pf2 --size=34 /usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf
 
 echo "Paste into file /etc/default/grub:"
