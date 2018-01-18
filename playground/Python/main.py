ad7f824b4ef314ff6c2e5a521f496234884e51fd
Szymon Bugaj <s.bugaj@samsung.com>
2018-01-18 12:32:02 +0100

    *** automatic sync commit ***

---
+++ playground/Python/main.py
@@ -0,0 +1,13 @@
+class Writer(dict):
+
+    """Some shitty class"""
+
+    def __init__(self):
+        """nothing more interesting """
+        dict.__init__(self)
+
+    def __lt__(self):
+
+    def shit(self):
+        
+    
