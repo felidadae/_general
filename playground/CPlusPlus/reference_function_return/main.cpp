23ffb551a659d1bfea1343eaa5dc969453631e0e
Szymon Bugaj <szymon.bugaj@protonmail.com>
2018-05-18 08:51:44 +0200

    *** automatic sync commit ***

--- playground/CPlusPlus/reference_function_return/main.cpp
+++ playground/CPlusPlus/reference_function_return/main.cpp
@@ -10,9 +10,16 @@ string& makeMyShit() {
     return mystring;
 }
 
+void myShit() {
+    std::string s;
+    const std::string& my_loved = new std::string("zupencja");
+    std::string& my_loved_2 = s;
+    return;
+}
+
 int main()
 {
-    string& mylocalstring(makeMyShit()); 
-    cout << mylocalstring; 
+    string& mylocalstring(makeMyShit());
+    cout << mylocalstring;
     return 0;
 }
