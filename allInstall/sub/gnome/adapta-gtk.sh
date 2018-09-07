f9fc6aec6aae43db9c1393a855d6d998091fd465
Szymon Bugaj <szymon.bugaj@intel.com>
2018-09-07 20:21:45 +0200

    *** automatic sync commit ***

--- allInstall/sub/gnome/adapta-gtk.sh
+++ allInstall/sub/gnome/adapta-gtk.sh
@@ -3,11 +3,12 @@ sudo apt-add-repository ppa:tista/adapta
 sudo apt update && sudo apt install -y adapta-gtk-theme
 
 mkdir -p ~/.themes
+rm -rf ~/.themes/Adapta-Nokto-My
 cp -r /usr/share/themes/Adapta-Nokto ~/.themes/Adapta-Nokto-My
 cd ~/.themes/Adapta-Nokto-My/gnome-shell
 
 #apply path
 perl -i -pe 'if (/^stage/) {s/font-family:.*?;/font-family: Ubuntu Condensed;/g;}' gnome-shell.css 
-perl -i -pe 'if (/^stage/) {s/font-size:.*?;/font-size: 11pt;/g;}' gnome-shell.css 
+perl -i -pe 'if (/^stage/) {s/font-size:.*?;/font-size: 10pt;/g;}' gnome-shell.css 
 #search entry rounded looks so badly
 perl -i -pe 'if (/^search-entry/) {s/border-radius:/s.*?;/border-radius: 0px;/g;}' gnome-shell.css 
