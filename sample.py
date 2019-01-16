5a56329f7c3375371d5402ef7565b9e1d19aec6d
Bugaj, Szymon <szymon.bugaj@intel.com>
2019-01-16 22:07:31 +0100

    *** automatic sync commit ***

---
+++ sample.py
@@ -0,0 +1,11 @@
+class Sample(object):
+
+    """Docstring for Sample. """
+    C=13
+    A=20
+
+    def __init__(self, x, y):
+        """TODO: to be defined1. """
+        self.x = x
+        self.y = y
+        
