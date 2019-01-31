6c477243c4f441e0eafffbf1734fec455f28be94
Bugaj, Szymon <szymon.bugaj@intel.com>
2019-01-31 22:31:13 +0100

    *** automatic sync commit ***

--- allInstall/centos_zsh.sh
+++ allInstall/centos_zsh.sh
@@ -1,18 +1,14 @@
 #!/bin/bash -ex
 
 # works for centos 7.5
+perl -i -pe 's/ZSH_THEME="felidadae"/ZSH_THEME="blinks"/' ~/.zshrc
+rm ~/.vimrc
+cd ..
 sudo yum -y install zsh git vim curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cd ~; mkdir Programming; cd Programming;
 git clone https://github.com/felidadae/_general.git
 mv _general _General
-cd _General
-cd dotfiles
-mkdir ~/.vim
-bash install.sh
-perl -i -pe 's/ZSH_THEME="felidadae"/ZSH_THEME="blinks"/' ~/.zshrc
-rm ~/.vimrc
-cd ..
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 sudo passwd sbugaj
